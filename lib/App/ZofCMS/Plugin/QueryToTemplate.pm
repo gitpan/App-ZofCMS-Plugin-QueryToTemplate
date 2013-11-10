0b92251f437e526594c8706e03cf0b1de6cf4b1b
Zoffix Znet <zoffixcpan@gmail.com>
2013-11-10 01:09:51 +0000

    Import of ZOFFIX/App-ZofCMS-Plugin-QueryToTemplate-0.0103 from CPAN.
    gitpan-cpan-distribution: App-ZofCMS-Plugin-QueryToTemplate
    gitpan-cpan-version:      0.0103
    gitpan-cpan-path:         ZOFFIX/App-ZofCMS-Plugin-QueryToTemplate-0.0103.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- lib/App/ZofCMS/Plugin/QueryToTemplate.pm
+++ lib/App/ZofCMS/Plugin/QueryToTemplate.pm
@@ -3,7 +3,7 @@ package App::ZofCMS::Plugin::QueryToTemplate;
 use warnings;
 use strict;
 
-our $VERSION = '0.0102';
+our $VERSION = '0.0103';
 
 sub new { bless {}, shift; }
 
@@ -20,6 +20,8 @@ sub process {
 
 1;
 __END__
+
+=encoding utf8
 
 =head1 NAME
 
